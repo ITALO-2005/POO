e9b8016c56a7425d0d2d8fa8cf15890110a3a783
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2025-03-26 19:20:54 +0000

    Teste4

--- dificeis.py
+++ dificeis.py
@@ -11,3 +11,31 @@ class Termostato:
 
 termostato = Termostato(30, 24, 10)
 termostato.exibir_info()
+
+class DNA:
+    def __init__(self, Genes, Cromossomos, Genoma):
+        self.Genes = Genes
+        self.Cromossomos = Cromossomos
+        self.Genoma = Genoma
+    
+    def exibir_info(self):
+        print(self.Genes)
+        print(self.Cromossomos)
+        print(self.Genoma)
+    
+dna = DNA("RNA", "autossomicos", "codificada_DNA")
+dna.exibir_info()
+        
+class Pixel:
+    def __init__(self, posicao_x, posicao_y, cor):
+        self.posicao_x = posicao_x  
+        self.posicao_y = posicao_y 
+        self.cor = cor 
+    
+    def exbir_info(self):
+        print(self.posicao_x)
+        print(self.posicao_y)
+        print(self.cor)
+
+pixel = Pixel(60, 50, "Black")
+pixel.exbir_info()
