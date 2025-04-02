0db11c04077ee7c57d16b4ff7d88b3ea03f6b205
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2025-04-02 16:45:42 +0000

    teste2

--- testes.py
+++ testes.py
@@ -1,3 +1,15 @@
+import random
+
+class Personagem:
+    def __init__(self, nome):
+        self.nome = nome
+        self.vida = 100
+
+    def atacar(self, alvo):
+        dano = random.randint(5, 20)
+        alvo.vida -= dano
+        print(f"{self.nome} ataca {alvo.nome} causando {dano} de dano!")
+
 class Inimigo:
     def __init__(self, nome):
         self.nome = nome
