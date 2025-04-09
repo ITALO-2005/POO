b974dbf36a2008121b6ef53047e4b06dd3b125ed
ITALO-2005 <italo-santos.is@academico.ifpb.edu.br>
2025-04-09 18:18:30 +0000

    Teste4

---
+++ livro.py
@@ -0,0 +1,21 @@
+class Livro:
+    def __init__(self, titulo, autor, isbn, copias_totais):
+        self.titulo = ""
+        self.autor = " "
+        self.isbn = ""
+        self.copias_totais = 0
+        self.copias_disponiveis = 0
+        self.emprestimos = []
+    
+    def get_emprestimos(self):
+        return self.__emprestimos
+    def emprestar(self, emprestimo):
+        if self.get_quantidade_disponivel () > 0:
+            self.__emprestimos.append(emprestimo)
+        else:
+            print(f"Não há livro disponivel para emprestimo")
+
+    def remover_emprestimo(self, emprestimo):
+        self.__emprestimos.remove(emprestimo)
+    def get_quantidade_disponivel(self):
+        return self.copias_totais - len(self.__emprestimos)
